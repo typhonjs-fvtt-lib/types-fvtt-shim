34c40f2d75f123003575a933c16ba83aa965a1ed
typhonrt <support@typhonjs.io>
2025-05-30 12:38:06 -0700

    v13 update.

--- dist/guard/index.d.ts
+++ dist/guard/index.d.ts
@@ -1,8 +1,43 @@
+/**
+ * Provides TypeScript type guards for several Foundry document model classes and objects.
+ *
+ * @packageDocumentation
+ */
+/**
+ * @param doc - A potential Foundry document.
+ *
+ * @returns Whether the given object is a Foundry document.
+ */
 declare function isDocument(doc: unknown): doc is fvtt.Document;
+/**
+ * @param doc - A potential Foundry document class.
+ *
+ * @returns Whether the given object is a Foundry document class.
+ */
 declare function isDocumentClass(doc: unknown): doc is fvtt.DocumentConstructor;
+/**
+ * @param collection - A potential Foundry document collection.
+ *
+ * @returns Whether the given object is a Foundry document collection.
+ */
 declare function isDocumentCollection(collection: unknown): collection is fvtt.DocumentCollection;
+/**
+ * @param collection - A potential Foundry embedded collection.
+ *
+ * @returns Whether the given object is a Foundry embedded collection.
+ */
 declare function isEmbeddedCollection(collection: unknown): collection is fvtt.EmbeddedCollection;
+/**
+ * @param doc - A potential Foundry folder.
+ *
+ * @returns Whether the given object is a Foundry folder.
+ */
 declare function isFolder(doc: unknown): doc is fvtt.Folder;
+/**
+ * @param doc - A potential Foundry folder class.
+ *
+ * @returns Whether the given object is a Foundry folder class.
+ */
 declare function isFolderClass(doc: unknown): doc is typeof fvtt.Folder;
 
 export { isDocument, isDocumentClass, isDocumentCollection, isEmbeddedCollection, isFolder, isFolderClass };
--- dist/guard/index.js
+++ dist/guard/index.js
@@ -1,20 +1,56 @@
+/**
+ * Provides TypeScript type guards for several Foundry document model classes and objects.
+ *
+ * @packageDocumentation
+ */
+/**
+ * @param doc - A potential Foundry document.
+ *
+ * @returns Whether the given object is a Foundry document.
+ */
 function isDocument(doc) {
     return doc !== void 0 && doc !== null && doc instanceof foundry.abstract.Document;
 }
+/**
+ * @param doc - A potential Foundry document class.
+ *
+ * @returns Whether the given object is a Foundry document class.
+ */
 function isDocumentClass(doc) {
     return doc !== void 0 && doc !== null && Object.prototype.isPrototypeOf.call(foundry.abstract.Document, doc);
 }
+/**
+ * @param collection - A potential Foundry document collection.
+ *
+ * @returns Whether the given object is a Foundry document collection.
+ */
 function isDocumentCollection(collection) {
-    return collection !== void 0 && collection !== null && collection instanceof DocumentCollection;
+    return collection !== void 0 && collection !== null &&
+        collection instanceof foundry.documents.abstract.DocumentCollection;
 }
+/**
+ * @param collection - A potential Foundry embedded collection.
+ *
+ * @returns Whether the given object is a Foundry embedded collection.
+ */
 function isEmbeddedCollection(collection) {
     return collection !== void 0 && collection !== null && collection instanceof foundry.abstract.EmbeddedCollection;
 }
+/**
+ * @param doc - A potential Foundry folder.
+ *
+ * @returns Whether the given object is a Foundry folder.
+ */
 function isFolder(doc) {
-    return doc !== void 0 && doc !== null && doc instanceof Folder;
+    return doc !== void 0 && doc !== null && doc instanceof foundry.documents.Folder;
 }
+/**
+ * @param doc - A potential Foundry folder class.
+ *
+ * @returns Whether the given object is a Foundry folder class.
+ */
 function isFolderClass(doc) {
-    return doc !== void 0 && doc !== null && Object.prototype.isPrototypeOf.call(Folder, doc);
+    return doc !== void 0 && doc !== null && Object.prototype.isPrototypeOf.call(foundry.documents.Folder, doc);
 }
 
 export { isDocument, isDocumentClass, isDocumentCollection, isEmbeddedCollection, isFolder, isFolderClass };
